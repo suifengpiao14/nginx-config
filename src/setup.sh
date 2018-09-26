1df0ee6746dfe72bb971c7392ac8a420d10a0c05
pengzheng <pengzheng@szprize.com>
2018-09-26 14:47:27 +0800

    优化脚本

--- src/setup.sh
+++ src/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 # 第一个参数为项目路径,默认为当前目录，第二个参数为域名，默认为项目路径文件名
-document_root=${1:-`pwd`}
-domain=${2:-` basename $document_root`}
-
+root_dir=`pwd`
+document_root=${1:-"${root_dir}/public"}
+domain=${2:-` basename $root_dir`}
 
 config_file="${domain}.conf";
 
-if [ -e config_file ];
-then
-    CONF_NGINX_EXISTS=1
-else
-    CONF_NGINX_EXISTS=0
+if [ -e ${config_file} ];then
+	echo "nginx config  is already exists!"
+	exit 0;
 fi
 
 cp ./vendor/suifengpiao14/nginx-config/src/domain.conf ${config_file}
@@ -19,10 +17,6 @@ cp ./vendor/suifengpiao14/nginx-config/src/domain.conf ${config_file}
 sed -i "s#{document_root}#${document_root}#g" ${config_file}
 sed -i "s#{domain}#${domain}#g" ${config_file}
 
+echo "nginx config is installed!"
 
-if [ "$CONF_NGINX_EXISTS" = 0 ];
-then
-    echo "auto config nginx script is installed!"
-else
-    echo "auto config nginx script is updated!"
-fi
+exit 0
