055e99703e81f6a89d2e390b05ce3472573333ca
pengzheng <pengzheng@szprize.com>
2018-10-26 15:39:31 +0800

    add cross_origin 、openresty valitator lua script

--- src/setup.sh
+++ src/setup.sh
@@ -3,6 +3,15 @@
 root_dir=`pwd`
 document_root=${1:-"${root_dir}/public"}
 domain=${2:-` basename $root_dir`}
+root_domain=${domain}
+
+# 针对二级域名，获取一级域名
+comma=${root_domain//[^.]}
+comma_count=${#comma}
+if [[  ${comma_count} -gt 1 ]]; then
+    root_domain=${root_domain#*.}
+fi
+
 
 config_file="${domain}.conf";
 
@@ -12,10 +21,12 @@ if [ -e ${config_file} ];then
 fi
 
 cp ./vendor/suifengpiao14/nginx-config/src/domain.conf ${config_file}
+cp ./vendor/suifengpiao14/nginx-config/src/cross_origin.conf cross_origin.conf
 
 #替换模板变量
 sed -i "s#{document_root}#${document_root}#g" ${config_file}
 sed -i "s#{domain}#${domain}#g" ${config_file}
+sed -i "s#{domain}#${root_domain}#g" cross_origin.conf
 
 echo "nginx config is installed!"
 
